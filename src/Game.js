7391b4bac23586cf404083b5b30549568e6751a7
Szymon Przybylski <szymon.przybylski@gmail.com>
2013-07-02 08:02:56 +0200

    starting point

--- src/Game.js
+++ src/Game.js
@@ -1,31 +1,6 @@
 define(function () {
     var Game = function () {
-        this.throws = [];
-    };
 
-    Game.prototype.roll = function (pins) {
-        this.throws.push(pins);
-    };
-
-    Game.prototype.getScore = function () {
-        var score = 0;
-
-        for (var i = 0; i < this.throws.length - 1;) {
-            if (this.throws[i] === 10) { // strike
-                if (this.throws[i + 2]) {
-                    score = score + 10 + this.throws[i + 1] + this.throws[i + 2];
-                }
-                i += 1;
-            } else if ((this.throws[i] + this.throws[i + 1]) === 10) { // spare
-                score = score + 10 + this.throws[i + 2];
-                i += 2;
-            } else {
-                score = score + this.throws[i] + this.throws[i + 1];
-                i += 2;
-            }
-        }
-
-        return score;
     };
 
     return Game;
--- tests/GameTest.js
+++ tests/GameTest.js
@@ -12,51 +12,6 @@ define(
             setup(function () {
                 this.game = new Game();
             });
-
-            test('Gutter game scores zero', function () {
-                for (var i = 1; i <= 20; i++) {
-                    this.game.roll(0);
-                }
-
-                assert.equal(0, this.game.getScore())
-            });
-
-            test('All onces scores 20', function () {
-                for (var i = 1; i <= 20; i++) {
-                    this.game.roll(1);
-                }
-
-                assert.equal(20, this.game.getScore());
-            });
-
-            test('One spare', function () {
-                this.game.roll(5);
-                this.game.roll(5);
-
-                for (var i = 1; i <= 18; i++) {
-                    this.game.roll(1);
-                }
-
-                assert.equal(10 + 1 + 18, this.game.getScore());
-            });
-
-            test('One strike', function () {
-                this.game.roll(10);
-
-                for (var i = 1; i <= 18; i++) {
-                    this.game.roll(1);
-                }
-
-                assert.equal(10 + 1 + 1 + 18, this.game.getScore());
-            });
-
-            test('Max score', function () {
-                for (var i = 1; i <= 12; i++) {
-                    this.game.roll(10);
-                }
-
-                assert.equal(300, this.game.getScore());
-            });
         });
     }
 );
